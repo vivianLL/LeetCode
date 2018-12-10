ea416a8992455ebb6cc9030bef2e5801be0c57de
vivianLL <978053846@qq.com>
2018-12-10 19:45:14 +0800

    Update 106_BuildTreeFromInandPostorder.py

--- 106_BuildTreeFromInandPostorder.py
+++ 106_BuildTreeFromInandPostorder.py
@@ -1,8 +1,8 @@
 '''
-105. Construct Binary Tree from Preorder and Inorder Traversal
-https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
+106. Construct Binary Tree from Inorder and Postorder Traversal
+https://leetcode.com/problems/construct-binary-tree-from-inorder-and-postorder-traversal/
 
-Given preorder and inorder traversal of a tree, construct the binary tree.
+Given inorder and postorder traversal of a tree, construct the binary tree.
 Note:
 You may assume that duplicates do not exist in the tree.
 '''
