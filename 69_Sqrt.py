84e24f5f21fafe75126bacca95c3fef5d186b032
vivianLL <978053846@qq.com>
2019-03-19 20:59:04 +0800

    求开方（Sqrt）

--- 69_Sqrt.py
+++ 69_Sqrt.py
@@ -6,6 +6,7 @@ Implement int sqrt(int x).
 Compute and return the square root of x, where x is guaranteed to be a non-negative integer.
 Since the return type is an integer, the decimal digits are truncated and only the integer part of the result is returned.
 '''
+import math
 class Solution:
     def mySqrt(self, x):
         # # 二分查找 关键在于结果的向下取整
@@ -29,21 +30,38 @@ class Solution:
         # elif h < l:
         #     return h
 
-        if x == 0:
-            return 0
-        l = 1
-        r = x
+        # if x == 0:
+        #     return 0
+        # l = 1
+        # r = x
+        #
+        # while l <= x:
+        #     res = (l + r) // 2
+        #     s = res ** 2
+        #
+        #     if s <= x < (res + 1) ** 2:   # 在判断平方时可以优化 因为向下取整
+        #         return res
+        #     if s < x:
+        #         l = res
+        #     if s > x:
+        #         r = res
 
-        while l <= x:
-            res = (l + r) // 2
-            s = res ** 2
+        # 牛顿法
+        # if x < 1e-6:
+        #     return 0
+        # last = x
+        # c = x / 2
+        # while math.fabs(c - last) > 1e-6:
+        #     last = c
+        #     c = (c + x / 2) / 2
+        # return c
 
-            if s <= x < (res + 1) ** 2:   # 在判断平方时可以优化 因为向下取整
-                return res
-            if s < x:
-                l = res
-            if s > x:
-                r = res
+        lr = 1e-6  # lr是学习率和最小误差
+        a = x / 2
+        while (a * a - x) > lr:
+            print(a)
+            a -= lr
+        return a
 
 
         # 自己写的 可以求浮点数的平方
@@ -76,5 +94,12 @@ class Solution:
 
 
 sol = Solution()
-ans = sol.mySqrt(6)
+ans = sol.mySqrt(9)
 print(ans)
+
+# 牛顿法思路：f(x)=x2−a
+# 所以f(x)的一次导是：
+# f′(x)=2x
+# 牛顿迭代式：
+# xn+1 = xn-f(xn)/f'(xn) = xn-(xn^2-a)/2xn
+# xn+1=1/2(xn+a/xn)
