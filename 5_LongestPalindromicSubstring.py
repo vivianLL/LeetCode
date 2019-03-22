a88c4cda22a488007760a5bf14f264a8cbaa33e3
vivianLL <978053846@qq.com>
2019-03-22 20:31:33 +0800

    最长回文子串

--- 5_LongestPalindromicSubstring.py
+++ 5_LongestPalindromicSubstring.py
@@ -21,7 +21,33 @@ class Solution:
         #                 maxstr = s[i:j + 1]
         # return maxstr
 
-        # 动态规划
+        # 动态规划 n*n的二维矩阵用于存储f(i,j)的值.f(i,j)表示s[i:j]子串是否是回文串。当j-i<=1时，如果s[i] == s[j]则表示s[i:j]为回文串
+        # f(i,j) = (s(i)==s(j)    , j-i<=1
+        # f(i,j) = (s(i)==s(j) and f(i+1,j-1)    ,j-i>1
+        k = len(s) # 计算字符串的长度
+        matrix = [[0 for i in range(k)] for i in range(k)] # 初始化n*n的列表
+        logestSubStr = ""  # 存储最长回文子串
+        logestLen = 0      # 最长回文子串的长度
+
+        for j in range(0, k):
+            for i in range(0, j + 1):             # 之所以是j+1是因为i可以等于j
+                if j - i <= 1:
+                    if s[i] == s[j]:
+                        matrix[i][j] = 1           # 此时f(i,j)置为true
+                        if logestLen < j - i + 1:   # 将s[i:j]的长度与当前的回文子串的最长长度相比 
+                            logestSubStr = s[i:j + 1]  # 取当前的最长回文子串
+                            logestLen = j - i + 1  # 当前最长回文子串的长度
+            else:
+                if s[i] == s[j] and matrix[i + 1][j - 1]: # 判断
+                    matrix[i][j] = 1
+                    if logestLen < j - i + 1:
+                        logestSubStr = s[i:j + 1]
+                        logestLen = j - i + 1
+        return logestSubStr
+
+
+
+        # 动态规划 O(n)
         n = len(s)
         maxl = 0
         start = 0
